fcfab407f489b940c6e01c6083972878595bb292
digitalight <mikeglover06@gmail.com>
2021-01-30 12:18:57 -0600

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # fs-uae-pi-install-script
 Script to install FS-UAE on to a fresh install of Raspberry Pi OS.
+
+
+## How to install FS-UAE 3.0.5 on Raspberry Pi
+-------------------------------------------
+
+The script does the following:
+
+### Debian Backports
+echo 'deb http://deb.debian.org/debian buster-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+
+### Add lha support
+sudo touch /etc/apt/sources.list.d/FrodeSolheim-stable.list
+
+echo 'deb https://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Raspbian_10/ /' | sudo tee -a /etc/apt/sources.list.d/FrodeSolheim-stable.list
+
+### Install FS-UAE
+sudo apt update
+
+sudo apt install -y -t buster-backports fs-uae fs-uae-launcher
+
+sudo apt install -y python3-lhafile
---
+++ fs-uae-pi-install-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# BASH SCRIPT TO INSTALL LATEST FS-UAE on Raspberry Pi
+
+
+REQUIRED_PKG="fs-uae"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "install ok installed" = "$PKG_OK" ]; then
+  echo "FS-UAE is already installed. Please remove first."
+  exit 
+fi
+if [ "" = "$PKG_OK" ]; then
+  echo "Installing FS-UAE"
+    if grep -q buster-backports "/etc/apt/sources.list"; then
+      echo "Buster Backports already added!"
+      exit
+    else
+      echo "Adding Buster Backports"
+      echo 'deb http://deb.debian.org/debian buster-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list
+      sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+      # Adding FS-UAE Repo
+      sudo touch /etc/apt/sources.list.d/FrodeSolheim-stable.list
+      echo 'deb https://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Raspbian_10/ /' | sudo tee -a /etc/apt/sources.list.d/FrodeSolheim-stable.list
+      sudo apt update
+      sudo apt install -y -t buster-backports fs-uae fs-uae-launcher
+      sudo apt install -y python3-lhafile
+      echo "Installation Complete"
+    fi
+  exit 
+fi
+
