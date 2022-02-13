791f48202eed9df9bcfd94be3e56dc5517976cc8
Mike Glover <mikeglover06@gmail.com>
2022-02-13 16:19:39 +0000

    adding bullseye, buster logic
    for new version of raspberry pi os

--- fs-uae-pi-install-script.sh
+++ fs-uae-pi-install-script.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # BASH SCRIPT TO INSTALL LATEST FS-UAE on Raspberry Pi
 
+# Check Raspberry Pi and OS version
+MODEL=$(cat /proc/device-tree/model)
+ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')S
 
+# Check if FS-UAE is already installed
 REQUIRED_PKG="fs-uae"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "install ok installed" = "$PKG_OK" ]; then
   echo "FS-UAE is already installed. Please remove first."
-  exit 
+  exit
 fi
+# Start Install
 if [ "" = "$PKG_OK" ]; then
   echo "Installing FS-UAE"
+  # OS is Buster
+  if [ "$VERSION" = "10" ]; then
+    # check if backports is added already
     if grep -q buster-backports "/etc/apt/sources.list"; then
       echo "Buster Backports already added!"
       exit
@@ -21,13 +29,22 @@ if [ "" = "$PKG_OK" ]; then
       # Adding FS-UAE Repo
       sudo touch /etc/apt/sources.list.d/FrodeSolheim-stable.list
       echo 'deb https://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Raspbian_10/ /' | sudo tee -a /etc/apt/sources.list.d/FrodeSolheim-stable.list
-      wget https://download.opensuse.org/repositories/home:FrodeSolheim:stable/Debian_10/Release.key
-      sudo apt-key add - < Release.key
+      curl -fsSL https://download.opensuse.org/repositories/home:FrodeSolheim:stable/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_FrodeSolheim_stable.gpg > /dev/null
       sudo apt update
       sudo apt install -y -t buster-backports fs-uae fs-uae-launcher
       sudo apt install -y python3-lhafile
       echo "Installation Complete"
     fi
-  exit 
+  # OS is Bullseye
+  elif [ "$VERSION" = "11" ]; then
+    sudo touch /etc/apt/sources.list.d/FrodeSolheim-stable.list
+    echo 'deb http://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/FrodeSolheim:stable.list
+    curl -fsSL https://download.opensuse.org/repositories/home:FrodeSolheim:stable/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_FrodeSolheim_stable.gpg > /dev/null
+    sudo apt update
+    sudo apt install -y fs-uae fs-uae-launcher python3-lhafile
+  else
+    echo "Version detected is not Buster or Bullseye, stopping script"
+    exit
+  fi
+  exit
 fi
-
